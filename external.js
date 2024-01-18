e80687879d83d9a87acbfa091e84230f38c038dd
Ibraheem1572 <abdulazeezjss1a@gmail.com>
2024-01-18 11:19:56 -0800

    first commit

---
+++ external.js
@@ -0,0 +1,7 @@
+function sala() {
+    document.querySelector('h1').style.color= 'white';
+    document.querySelector('h1').innerHTML = 'IT IS JAVASCRIPT CLASS';
+    document.querySelector('img').src='solo.jpg';
+    document.querySelector('body').style.background='green';
+    confirm('are you sure you want to logout?')
+}
